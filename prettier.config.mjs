5a1ed78a81e822116a7d74577a5a2314621ee7c6
0seo8 <00seo23@gmail.com>
2025-04-16 13:24:20 +0900

    settings: 프로젝트 초기 세팅
    - 문서(README, PRD) 추가
    - 관련 패키지(React Query, Recoil 등) 설치
    - eslint, prettier 설정
    - next.config 설정

---
+++ prettier.config.mjs
@@ -0,0 +1,14 @@
+/**
+ * @see https://prettier.io/docs/en/configuration.html
+ * @type {import("prettier").Config}
+ */
+const config = {
+  printWidth: 120,
+  tabWidth: 2,
+  semi: true,
+  trailingComma: 'all',
+  singleQuote: true,
+  arrowParens: 'always',
+};
+
+export default config;
